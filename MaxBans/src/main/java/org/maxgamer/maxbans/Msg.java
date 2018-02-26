1fb6a497951c598127454ed36c3e6fc31593f991
Dave <david.tkachuk@hotmail.com>
2018-02-26 15:31:30 -0500

    fixing message class

--- MaxBans/src/main/java/org/maxgamer/maxbans/Msg.java
+++ MaxBans/src/main/java/org/maxgamer/maxbans/Msg.java
@@ -2,16 +2,6 @@ package org.maxgamer.maxbans;
 
 import org.bukkit.ChatColor;
 
-import java.io.InputStream;
-import java.io.IOException;
-
-import org.bukkit.configuration.InvalidConfigurationException;
-
-import java.io.FileNotFoundException;
-
-import org.bukkit.configuration.Configuration;
-
-import java.io.FileOutputStream;
 import java.io.File;
 
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -22,44 +12,9 @@ public class Msg
     
     @SuppressWarnings("deprecation")
 	public static void reload() {
-        final File f = new File(MaxBans.instance.getDataFolder(), "messages.yml");
-        Msg.cfg = new YamlConfiguration();
-        try {
-            final YamlConfiguration defaults = new YamlConfiguration();
-            InputStream in = MaxBans.instance.getResource("messages.yml");
-            defaults.load(in);
-            in.close();
-            if (f.exists()) {
-                Msg.cfg.load(f);
-            }
-            else {
-                final FileOutputStream out = new FileOutputStream(f);
-                in = MaxBans.instance.getResource("messages.yml");
-                final byte[] buffer = new byte[1024];
-                for (int len = in.read(buffer); len != -1; len = in.read(buffer)) {
-                    out.write(buffer, 0, len);
-                }
-                in.close();
-                out.close();
-            }
-            Msg.cfg.setDefaults((Configuration)defaults);
-        }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        catch (InvalidConfigurationException e2) {
-            e2.printStackTrace();
-            System.out.println("Invalid messages.yml config. Using defaults.");
-            try {
-                Msg.cfg.load(MaxBans.instance.getResource("messages.yml"));
-            }
-            catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-        catch (IOException e3) {
-            e3.printStackTrace();
-        }
+        final File f = new File(MaxBans.instance.getDataFolder() + "/messages.yml");
+        if (!f.exists()) MaxBans.instance.saveResource("messages.yml", false);
+        Msg.cfg = YamlConfiguration.loadConfiguration(f);
     }
     
     public static String get(final String loc, final String[] keys, final String[] values) {
